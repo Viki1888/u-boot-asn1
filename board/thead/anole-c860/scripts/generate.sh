d04c9064b5566efe50633ff85743854aae95d1c9
Liu Yibin <jiulong@linux.alibaba.com>
2020-07-28 17:41:23 +0800

    Update scripts for anole c860
    Signed-off-by: Liu Yibin <jiulong@linux.alibaba.com>

---
+++ board/thead/anole-c860/scripts/generate.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "Generating mboot image ..."
+sh ./mboot.sh ../../../../spl/u-boot-spl.bin ../../../..//u-boot.bin .
+
+echo "Generating uboot_ram image ..."
+dtc -I dts -O dtb ../../../../arch/csky/dts/anole-rgmii-c860.dts > dt.dtb
+bash ./multi_bin.sh ./uboot_ram.img  ./dt.dtb ./u-boot.bin
--- board/thead/anole-c860/scripts/x.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "Generating mboot image ..."
-sh ./mboot.sh ./u-boot-spl.bin ./u-boot.bin .
-
-echo "Generating uboot_ram image ..."
-bash ./multi_bin.sh ./uboot_ram.img  ./dt.dtb ./u-boot.bin
