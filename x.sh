e43a9f109f1fc44daf0ebe0d65560c6e06cec3ab
Liu Yibin <jiulong@linux.alibaba.com>
2021-01-19 15:09:12 +0800

    Remove useless script & config

--- x.sh
+++
@@ -1,6 +0,0 @@
-make clean
-make distclean
-make ice_evb_c910_defconfig
-make ARCH=riscv CROSS_COMPILE=~/nixon/bin/riscv64-unknown-linux-gnu- -j
-
-~/nixon/bin/riscv64-unknown-linux-gnu-objdump -d u-boot > 123
