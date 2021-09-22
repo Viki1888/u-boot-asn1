0aaf027dddc9ffb20dcaeaad3cd391f9cdb236da
linghui.zlh <linghui.zlh@alibaba-inc.com>
2021-09-22 15:43:34 +0800

    board: thead: light-fpga-fm-c910: init confg spi/qspi0/qspi io capability and io-mux
    1) SPI0    config CS0 as gpio function, pull up all spi-pd except csn
    and clk pad
    2) QSPIO   config CS0 as gpio function, pull up all qspi0-pad except csn
    and clk pad
    3) QSPI1   config CS0 as gpio function, pull up all qspi1-pad except csn
    and clk pad
    
    Signed-off-by: linghui.zlh <linghui.zlh@alibaba-inc.com>

--- board/thead/light-fpga-fm-c910/light.c
+++ board/thead/light-fpga-fm-c910/light.c
@@ -22,6 +22,34 @@ void usb_clk_config(void)
 	writel(0x7, (void *)(0xffef01406c));
 }
 
+static void spi_hw_init(void)
+{
+
+/*spi0 cs0 gpio2-15 */
+#define SPI_CS0_PINMUX 0xffec007404
+	writel(readl((void *)SPI_CS0_PINMUX) | (0x3 << 28), (void *)SPI_CS0_PINMUX);
+/*set spi0 driving strength and pull-up */
+	writel(readl((void *)(0xffec00701c)) | (0x135 | (0x135 <<16)), (void *)(0xffec00701c));
+	writel(readl((void *)(0xffec007020)) | (0x135 | (0x135 <<16)), (void *)(0xffec007020));
+
+/*qspi0 cs0 gpio2-3 */
+#define QSPI0_CS0_PINMUX 0xffec007400
+	writel(readl((void *)QSPI0_CS0_PINMUX) | (0x3 << 12), (void *)QSPI0_CS0_PINMUX);
+/*set qspi1 driving strength and pull-up */
+	writel(readl((void *)(0xffec007004)) | (0x108 | (0x108 <<16)), (void *)(0xffec007004));
+	writel(readl((void *)(0xffec007008)) | (0x138 | (0x138 <<16)), (void *)(0xffec007008));
+	writel(readl((void *)(0xffec00700c)) | (0x138 | (0x138 <<16)), (void *)(0xffec00700c));
+
+/*qspi1 cs0 gpio0-1 */
+#define QSPI1_CS0_PINMUX 0xffe7f3c400
+	writel(readl((void *)QSPI1_CS0_PINMUX) | (0x3 << 4), (void *)QSPI1_CS0_PINMUX);
+/*set qspi1 driving strength and pull-up */
+	writel(readl((void *)(0xffe7f3c000)) | (0x108 | (0x108 <<16)), (void *)(0xffe7f3c000));
+	writel(readl((void *)(0xffe7f3c004)) | (0x138 | (0x138 <<16)), (void *)(0xffe7f3c004));
+	writel(readl((void *)(0xffe7f3c008)) | (0x138 | (0x138 <<16)), (void *)(0xffe7f3c008));
+
+}
+
 int board_init(void)
 {
 #define GMAC_IOPMP   0xfff7038000
@@ -30,14 +58,9 @@ int board_init(void)
 #define USB_IOPMP   0xfff703a000
 	writel(0xffffffff, (void *)(USB_IOPMP+0xc0));
 
-#define SPI_NOR_FLASH_PINMUX 0xffef01c430
-	writel(readl((void *)SPI_NOR_FLASH_PINMUX) | 0x30, (void *)SPI_NOR_FLASH_PINMUX);
-
-#define SPI_NAND_FLASH_PINMUX 0xffef01c428
-	writel(readl((void *)SPI_NAND_FLASH_PINMUX) | (3<<20), (void *)SPI_NAND_FLASH_PINMUX);
-
 	usb_clk_config();
 	gmac_clk_config();
+    spi_hw_init();
 
 	return 0;
 }
