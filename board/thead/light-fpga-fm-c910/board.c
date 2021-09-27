283a906cf07e14229331811d7e5716da94022e4e
fugang.duan <duanfugang.dfg@alibaba-inc.com>
2021-09-27 15:13:47 +0800

    board: light: add e902 firmware boot
    Add light e902 firmware boot.
    
    Signed-off-by: fugang.duan <duanfugang.dfg@alibaba-inc.com>

--- board/thead/light-fpga-fm-c910/board.c
+++ board/thead/light-fpga-fm-c910/board.c
@@ -5,6 +5,7 @@
  */
 
 #include <common.h>
+#include <asm/io.h>
 #include <dwc3-uboot.h>
 #include <usb.h>
 
@@ -37,4 +38,39 @@ int g_dnl_board_usb_cable_connected(void)
 {
 	return 1;
 }
+
+#ifdef CONFIG_CMD_BOOT_SLAVE
+#define E902_SYSREG_START	0xfffff48044
+#define E902_SYSREG_RESET	0xfffff44024
+#define E902_START_ADDRESS	0xFFEF8000
+#define C910_E902_START_ADDRESS 0xFFFFEF8000
+#define E902_IOPMP_BASE		0xFFFFC21000
+
+void set_slave_cpu_entry(phys_addr_t entry)
+{
+    writel(entry, (void *)E902_SYSREG_START);
+}
+
+void disable_slave_cpu(void)
+{
+    writel(0x2, (void *)E902_SYSREG_RESET);
+}
+
+void enable_slave_cpu(void)
+{
+    writel(0x3, (void *)E902_SYSREG_RESET);
+}
+
+int do_bootslave(cmd_tbl_t * cmdtp, int flag, int argc, char * const argv[])
+{
+	writel(0xffffffff, (void *)(E902_IOPMP_BASE + 0xc0));
+	disable_slave_cpu();
+	set_slave_cpu_entry(E902_START_ADDRESS);
+	flush_cache((uintptr_t)C910_E902_START_ADDRESS, 0x10000);
+	enable_slave_cpu();
+
+	return 0;
+}
+#endif
+
 #endif
