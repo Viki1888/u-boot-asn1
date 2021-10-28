Viki1888/u-boot-asn1

commit f275883128addda2024a27183a002f600a5631c9

board: thead: support init all soc pin config
support light-soc pin init configuration all

Signed-off-by: linghui.zlh < linghui.zlh@alibaba-inc.com >

board/thead/light-c910/light.c
@@ -11,16 +11,428 @@
 #include <linux/bitops.h>
 #include <asm/arch-thead/light-iopmp.h>
 
+#define SOC_PIN_AP_RIGHT_TOP          (0x0)
+#define SOC_PIN_AP_LEFT_TOP           (0x1)
+#define SOC_PIN_AON                   (0x2)
+
+#define PAD_GRP_BASE_SET(x)           (x << 12)
+#define PAD_GRP_IDX_GET(x)            ( (x >> 12) & 0xF)
+
+typedef enum {
+	UART0_TXD = PAD_GRP_BASE_SET(SOC_PIN_AP_RIGHT_TOP),
+	UART0_RXD,
+	QSPI0_SCLK,
+	QSPI0_CSN0,
+	QSPI0_CSN1,
+	QSPI0_D0_MOSI,
+	QSPI0_D1_MISO,
+	QSPI0_D2_WP,
+	QSPI0_D3_HOLD,
+	I2C2_SCL,
+	I2C2_SDA,
+	I2C3_SCL,
+	I2C3_SDA,
+	GPIO2_13,
+	SPI_SCLK,
+	SPI_CSN,
+	SPI_MOSI,
+	SPI_MISO,
+	GPIO2_18,
+	GPIO2_19,
+	GPIO2_20,
+	GPIO2_21,
+	GPIO2_22,
+	GPIO2_23,
+	GPIO2_24,
+	GPIO2_25,
+	SDIO0_WPRTN,
+	SDIO0_DETN,
+	SDIO1_WPRTN,
+	SDIO1_DETN,
+	GPIO2_30,
+	GPIO2_31,
+	GPIO3_0,
+	GPIO3_1,
+	GPIO3_2,
+	GPIO3_3,
+	HDMI_SCL,
+	HDMI_SDA,
+	HDMI_CEC,
+	GMAC0_TX_CLK,
+	GMAC0_RX_CLK,
+	GMAC0_TXEN,
+	GMAC0_TXD0,
+	GMAC0_TXD1,
+	GMAC0_TXD2,
+	GMAC0_TXD3,
+	GMAC0_RXDV,
+	GMAC0_RXD0,
+	GMAC0_RXD1,
+	GMAC0_RXD2,
+	GMAC0_RXD3,
+	GMAC0_MDC,
+	GMAC0_MDIO,
+	GMAC0_COL,
+	GMAC0_CRS,
+
+	QSPI1_SCLK = PAD_GRP_BASE_SET(SOC_PIN_AP_LEFT_TOP),
+	QSPI1_CSN0,
+	QSPI1_D0_MOSI,
+	QSPI1_D1_MISO,
+	QSPI1_D2_WP,
+	QSPI1_D3_HOLD,
+	I2C0_SCL,
+	I2C0_SDA,
+	I2C1_SCL,
+	I2C1_SDA,
+	UART1_TXD,
+	UART1_RXD,
+	UART4_TXD,
+	UART4_RXD,
+	UART4_CTSN,
+	UART4_RTSN,
+	UART3_TXD,
+	UART3_RXD,
+	GPIO0_18,
+	GPIO0_19,
+	GPIO0_20,
+	GPIO0_21,
+	GPIO0_22,
+	GPIO0_23,
+	GPIO0_24,
+	GPIO0_25,
+	GPIO0_26,
+	GPIO0_27,
+	GPIO0_28,
+	GPIO0_29,
+	GPIO0_30,
+	GPIO0_31,
+	GPIO1_0,
+	GPIO1_1,
+	GPIO1_2,
+	GPIO1_3,
+	GPIO1_4,
+	GPIO1_5,
+	GPIO1_6,
+	GPIO1_7,
+	GPIO1_8,
+	GPIO1_9,
+	GPIO1_10,
+	GPIO1_11,
+	GPIO1_12,
+	GPIO1_13,
+	GPIO1_14,
+	GPIO1_15,
+	GPIO1_16,
+	CLK_OUT_0,
+	CLK_OUT_1,
+	CLK_OUT_2,
+	CLK_OUT_3,
+	GPIO1_21,
+	GPIO1_22,
+	GPIO1_23,
+	GPIO1_24,
+	GPIO1_25,
+	GPIO1_26,
+	GPIO1_27,
+	GPIO1_28,
+	GPIO1_29,
+	GPIO1_30,
+	OSC_CLK_IN,
+
+	OSC_CLK_OUT= PAD_GRP_BASE_SET(SOC_PIN_AON),
+	SYS_RST_N,
+	TEST_MODE,
+	DFT_PROT_DIS_0,
+	DEBUG_MODE,
+	POR_SEL,
+	I2C_AON_SCL,
+	I2C_AON_SDA,
+	CPU_JTG_TCLK,
+	CPU_JTG_TMS,
+	CPU_JTG_TDI,
+	CPU_JTG_TDO,
+	CPU_JTG_TRST,
+	AOGPIO_7,
+	AOGPIO_8,
+	AOGPIO_9,
+	AOGPIO_10,
+	AOGPIO_11,
+	AOGPIO_12,
+	AOGPIO_13,
+	AOGPIO_14,
+	AOGPIO_15,
+	AUDIO_PA0,
+	AUDIO_PA1,
+	AUDIO_PA2,
+	AUDIO_PA3,
+	AUDIO_PA4,
+	AUDIO_PA5,
+	AUDIO_PA6,
+	AUDIO_PA7,
+	AUDIO_PA8,
+	AUDIO_PA9,
+	AUDIO_PA10,
+	AUDIO_PA11,
+	AUDIO_PA12,
+	AUDIO_PA13,
+	AUDIO_PA14,
+	AUDIO_PA15,
+	AUDIO_PA16,
+	AUDIO_PA17,
+	AUDIO_PA27,
+	AUDIO_PA28,
+	AUDIO_PA29,
+	AUDIO_PA30,
+} pin_name_t;
+
+enum {
+	PIN_PD,
+	PIN_PU,
+	PIN_PN,
+};
+
+enum {
+	PIN_SPEED_NORMAL,
+	PIN_SPEED_FAST
+};
+
+#define PIN_DRV_MAX             (15)
+
 #define SYSCLK_USB_CTRL		0xFFFC02D104
 #define REF_SSP_EN		0xFFEC03F034
 #define USB3_DRD_SWRST		0xFFEC02C014
 #define SOC_OM_ADDRBASE		0xFFEF018010
-#define QSPI1_CS0_PINMUX	0xFFE7f3c400
-#define QSPI0_CS0_PINMUX	0xFFEC007400
-#define SPI_CS0_PINMUX		0xFFEC007404
 
 #define LIGHT_IOPMP_DEFAULT_ATTR	0xffffffff
 #define LIGHT_IOPMP_DEFAULT_OFF		0xc0
+
+#define PADMUX_REG_ADDR(base,index)  ((void*)(base) + ((index >> 3) << 2))
+#define PADMUX_REG_BIT_POS(index)    ((index & 0x7) << 2)
+#define PADMUX_REG_MASK(index)       (0xF << PADMUX_REG_BIT_POS(index))
+
+#define PADCFG_REG_ADDR(base,index)  ((void*)(base) + ((index >> 1) << 2))
+#define PADCFG_REG_BIT_POS(index)    ((index & 0x1) << 4)
+#define PADCFG_REG_PULL_MASK(index)  (0x70 << PADCFG_REG_BIT_POS(index))
+#define PADCFG_REG_DRV_MASK(index)   (0xF << PADCFG_REG_BIT_POS(index))
+#define PADCFG_REG_SLEW_MASK(index) (0x100 << PADCFG_REG_BIT_POS(index))
+
+
+#define PADMUX_CFG(grp_addr_base,index,pin_func)  \
+        { \
+		uint32_t val; \
+		val = readl(PADMUX_REG_ADDR(grp_addr_base,index)); \
+		val &=~PADMUX_REG_MASK(index); \
+		val |=	pin_func << PADMUX_REG_BIT_POS(index); \
+		writel(val,PADMUX_REG_ADDR(grp_addr_base,index)); \
+	}
+
+#define PADCFG_PULL(grp_addr_base,index,bpull,bpullup)  \
+        { \
+		uint32_t val; \
+		val = readl(PADCFG_REG_ADDR(grp_addr_base,index)); \
+		val &=~PADCFG_REG_PULL_MASK(index); \
+		val |=	((bpull<<4)|(bpullup<<5)) << PADCFG_REG_BIT_POS(index); \
+		writel(val,PADCFG_REG_ADDR(grp_addr_base,index)); \
+	}
+
+#define PADCFG_SLEW(grp_addr_base,index,bfast)  \
+        { \
+		uint32_t val; \
+		val = readl(PADCFG_REG_ADDR(grp_addr_base,index)); \
+		val &=~PADCFG_REG_SLEW_MASK(index); \
+		val |=	(bfast<<8) << PADCFG_REG_BIT_POS(index); \
+		writel(val,PADCFG_REG_ADDR(grp_addr_base,index)); \
+	}
+
+#define PADCFG_DRV(grp_addr_base,index,strength)  \
+        { \
+		uint32_t val; \
+		val = readl(PADCFG_REG_ADDR(grp_addr_base,index)); \
+		val &=~PADCFG_REG_DRV_MASK(index); \
+		val |=	(strength) << PADCFG_REG_BIT_POS(index); \
+		writel(val,PADCFG_REG_ADDR(grp_addr_base,index)); \
+	}
+
+struct pinmux_addr_t {
+	uint32_t pin_grp_id;
+	uint32_t *pin_grp_cfg_base;
+	uint32_t *pin_grp_mux_base;
+};
+
+#define PIN_GRP_ADDR_DEF(grp_idx,cfg_base,mux_base) \
+{ \
+	.pin_grp_id = grp_idx, \
+	.pin_grp_cfg_base =(uint32_t*) cfg_base, \
+	.pin_grp_mux_base =(uint32_t*) mux_base, \
+}
+
+struct pinmux_addr_t g_soc_pin_grp_addr[] = {
+	PIN_GRP_ADDR_DEF(SOC_PIN_AP_RIGHT_TOP,      0xFFEC007000,0xFFEC007400),
+	PIN_GRP_ADDR_DEF(SOC_PIN_AP_LEFT_TOP,       0xFFE7F3C000,0xFFE7F3C400),
+	PIN_GRP_ADDR_DEF(SOC_PIN_AON,               0xFFFF4A000,0xFFFF4A400),
+};
+
+static int  light_pinmx_get_cfg_base(pin_name_t pin_name, uint32_t** cfg_base)
+{
+	uint32_t grp_num = ARRAY_SIZE(g_soc_pin_grp_addr);
+	uint32_t grp_idx = PAD_GRP_IDX_GET(pin_name);
+	uint32_t i;
+
+	for(i = 0; i < grp_num; i++) {
+		if (grp_idx == g_soc_pin_grp_addr[i].pin_grp_id)
+			break;
+	}
+
+	if (i == grp_num)
+		return -1;
+
+	*cfg_base = g_soc_pin_grp_addr[i].pin_grp_cfg_base;
+	return 0;
+}
+
+static int  light_pinmx_get_mux_base(pin_name_t pin_name, uint32_t** cfg_base)
+{
+	uint32_t grp_num = ARRAY_SIZE(g_soc_pin_grp_addr);
+	uint32_t grp_idx = PAD_GRP_IDX_GET(pin_name);
+	uint32_t i;
+
+	for(i = 0; i < grp_num; i++) {
+		if (grp_idx == g_soc_pin_grp_addr[i].pin_grp_id)
+			break;
+	}
+
+	if (i == grp_num)
+		return -1;
+
+	*cfg_base = g_soc_pin_grp_addr[i].pin_grp_mux_base;
+	return 0;
+}
+
+/*******************************************************************************
+ * function: danica_ioreuse_inital
+ *
+ * description:
+ *   initial danica_pinmux
+ *******************************************************************************/
+static inline int light_pin_pllmode(pin_name_t pin_name, uint32_t mode)
+{
+	uint32_t bpull,bpullup;
+	uint32_t *reg_addr;
+	int      ret;
+
+	ret = light_pinmx_get_cfg_base(pin_name,&reg_addr);
+	if (ret)
+		return ret;
+
+	switch (mode)
+	{
+	case PIN_PD:
+		bpull =1;
+		bpullup = 0;
+		/* code */
+		break;
+	case PIN_PU:
+		bpull =1;
+		bpullup = 1;
+		/* code */
+		break;
+	case PIN_PN:
+		bpull =0;
+		bpullup = 0;
+		/* code */
+		break;
+	default:
+		return -1;
+		break;
+	}
+	PADCFG_PULL(reg_addr,pin_name,bpull,bpullup);
+
+    return 0;
+}
+
+/**
+  \brief       set pin speed
+  \param[in]   pin_name pin name, defined in soc.h.
+  \param[in]   speed    io speed
+  \return      error code
+*/
+static inline int light_pin_speed(pin_name_t pin_name, uint32_t speed)
+{
+	uint32_t bfast;
+	uint32_t *reg_addr;
+	int      ret;
+
+	ret = light_pinmx_get_cfg_base(pin_name,&reg_addr);
+	if (ret)
+		return ret;
+
+	switch (speed)
+	{
+	case PIN_SPEED_NORMAL:
+		bfast = 0;
+		break;
+	case PIN_SPEED_FAST:
+		bfast = 1;
+		break;
+	default:
+		return -1;
+	}
+
+	PADCFG_SLEW(reg_addr,pin_name,bfast);
+    return 0;
+}
+
+/**
+  \brief       set pin drive
+  \param[in]   pin_name pin name, defined in soc.h.
+  \param[in]   drive    io drive
+  \return      error code
+*/
+static inline int light_pin_drv_strength(pin_name_t pin_name, uint32_t strength)
+{
+	uint32_t *reg_addr;
+	int      ret;
+
+	ret = light_pinmx_get_cfg_base(pin_name,&reg_addr);
+	if (ret)
+		return ret;
+
+	if(strength > PIN_DRV_MAX) {
+		return -1;
+	}
+
+	PADCFG_DRV(reg_addr,pin_name,strength);
+	return 0;
+}
+
+int light_pin_mux(pin_name_t pin_name, uint32_t pin_func)
+{
+	uint32_t *reg_addr;
+	int      ret;
+
+	ret = light_pinmx_get_mux_base(pin_name,&reg_addr);
+	if (ret)
+		return ret;
+
+	PADMUX_CFG(reg_addr,pin_name,pin_func);
+    return 0;
+}
+
+int light_pin_cfg(pin_name_t pin_name,uint32_t slew_rate, uint32_t pullmode,uint32_t drvstrength)
+{
+	int ret;
+
+	ret = light_pin_speed(pin_name,slew_rate);
+	if (ret)
+		return ret;
+	ret = light_pin_pllmode(pin_name,pullmode);
+	if (ret )
+		return ret;
+	ret = light_pin_drv_strength(pin_name,drvstrength);
+
+	return ret;
+}
 
 static struct light_reset_list {
         u32 val;
@@ -111,28 +523,110 @@ static void usb_clk_config(void)
 	writel(0x7, (void *)USB3_DRD_SWRST);
 }
 
-static void spi_hw_init(void)
+static void light_iopin_init(void)
 {
+	light_pin_cfg(I2C_AON_SCL,PIN_SPEED_NORMAL,PIN_PN,2);
+	light_pin_cfg(I2C_AON_SDA,PIN_SPEED_NORMAL,PIN_PN,2);
+	light_pin_cfg(AOGPIO_8,PIN_SPEED_NORMAL,PIN_PN,2);
+	light_pin_cfg(AOGPIO_9,PIN_SPEED_NORMAL,PIN_PN,2);
+	light_pin_mux(AOGPIO_10,1);
+	light_pin_mux(AOGPIO_11,1);
+	light_pin_mux(AOGPIO_12,1);
+	light_pin_mux(AOGPIO_13,1);
+	light_pin_mux(AUDIO_PA30,3);
 
-	/*spi0 cs0 gpio2-15 */
-	writel(readl((void *)SPI_CS0_PINMUX) | (0x3 << 28), (void *)SPI_CS0_PINMUX);
-	/*set spi0 driving strength and pull-up */
-	writel(readl((void *)(0xffec00701c)) | (0x135 | (0x135 <<16)), (void *)(0xffec00701c));
-	writel(readl((void *)(0xffec007020)) | (0x135 | (0x135 <<16)), (void *)(0xffec007020));
+	/*qspi1 cs0 gpio0-1 pad strength and pin-pull mode*/
+	light_pin_mux(QSPI1_CSN0,3);
+	light_pin_cfg(QSPI1_CSN0,PIN_SPEED_NORMAL,PIN_PN,8);
+	light_pin_cfg(QSPI1_SCLK,PIN_SPEED_NORMAL,PIN_PN,8);
+	light_pin_cfg(QSPI1_D0_MOSI,PIN_SPEED_NORMAL,PIN_PU,8);
+	light_pin_cfg(QSPI1_D1_MISO,PIN_SPEED_NORMAL,PIN_PU,8);
+	light_pin_cfg(QSPI1_D2_WP,PIN_SPEED_NORMAL,PIN_PU,8);
+	light_pin_cfg(QSPI1_D3_HOLD,PIN_SPEED_NORMAL,PIN_PU,8);
 
-	/*qspi0 cs0 gpio2-3 */
-	writel(readl((void *)QSPI0_CS0_PINMUX) | (0x3 << 12), (void *)QSPI0_CS0_PINMUX);
-	/*set qspi1 driving strength and pull-up */
-	writel(readl((void *)(0xffec007004)) | (0x108 | (0x108 <<16)), (void *)(0xffec007004));
-	writel(readl((void *)(0xffec007008)) | (0x138 | (0x138 <<16)), (void *)(0xffec007008));
-	writel(readl((void *)(0xffec00700c)) | (0x138 | (0x138 <<16)), (void *)(0xffec00700c));
+	light_pin_cfg(I2C0_SCL,PIN_SPEED_NORMAL,PIN_PN,4);
+	light_pin_cfg(I2C0_SDA,PIN_SPEED_NORMAL,PIN_PN,4);
+	light_pin_cfg(I2C1_SCL,PIN_SPEED_NORMAL,PIN_PN,4);
+	light_pin_cfg(I2C1_SDA,PIN_SPEED_NORMAL,PIN_PN,4);
 
-	/*qspi1 cs0 gpio0-1 */
-	writel(readl((void *)QSPI1_CS0_PINMUX) | (0x3 << 4), (void *)QSPI1_CS0_PINMUX);
-	/*set qspi1 driving strength and pull-up */
-	writel(readl((void *)(0xffe7f3c000)) | (0x108 | (0x108 <<16)), (void *)(0xffe7f3c000));
-	writel(readl((void *)(0xffe7f3c004)) | (0x138 | (0x138 <<16)), (void *)(0xffe7f3c004));
-	writel(readl((void *)(0xffe7f3c008)) | (0x138 | (0x138 <<16)), (void *)(0xffe7f3c008));
+	light_pin_mux(UART3_TXD,1);
+	light_pin_mux(UART3_RXD,1);
+	light_pin_cfg(UART3_TXD,PIN_SPEED_NORMAL,PIN_PN,2);
+	light_pin_cfg(UART3_RXD,PIN_SPEED_NORMAL,PIN_PN,2);
+
+
+	light_pin_mux(GPIO0_18,1);
+	light_pin_mux(GPIO0_19,1);
+	light_pin_cfg(GPIO0_18,PIN_SPEED_NORMAL,PIN_PN,4);
+	light_pin_cfg(GPIO0_19,PIN_SPEED_NORMAL,PIN_PN,4);
+
+	light_pin_mux(GPIO0_20,2);
+	light_pin_mux(GPIO0_21,2);
+	light_pin_cfg(GPIO0_20,PIN_SPEED_NORMAL,PIN_PN,2);
+	light_pin_cfg(GPIO0_21,PIN_SPEED_NORMAL,PIN_PN,2);
+
+
+	light_pin_mux(GPIO1_0,1);
+	light_pin_mux(GPIO1_1,1);
+	light_pin_mux(GPIO1_2,1);
+	light_pin_mux(GPIO1_3,1);
+	light_pin_mux(GPIO1_4,1);
+	light_pin_cfg(GPIO1_0,PIN_SPEED_NORMAL,PIN_PN,2);
+	light_pin_cfg(GPIO1_1,PIN_SPEED_NORMAL,PIN_PN,2);
+	light_pin_cfg(GPIO1_2,PIN_SPEED_NORMAL,PIN_PN,2);
+	light_pin_cfg(GPIO1_3,PIN_SPEED_NORMAL,PIN_PN,2);
+	light_pin_cfg(GPIO1_4,PIN_SPEED_NORMAL,PIN_PN,2);
+
+
+	light_pin_cfg(I2C2_SCL,PIN_SPEED_NORMAL,PIN_PN,4);
+	light_pin_cfg(I2C2_SDA,PIN_SPEED_NORMAL,PIN_PN,4);
+	light_pin_cfg(I2C3_SCL,PIN_SPEED_NORMAL,PIN_PN,4);
+	light_pin_cfg(I2C3_SDA,PIN_SPEED_NORMAL,PIN_PN,4);
+
+
+	light_pin_mux(GPIO2_18,1);
+	light_pin_mux(GPIO2_19,1);
+	light_pin_mux(GPIO2_20,1);
+	light_pin_mux(GPIO2_21,1);
+	light_pin_mux(GPIO2_22,1);
+	light_pin_mux(GPIO2_23,1);
+	light_pin_mux(GPIO2_24,1);
+	light_pin_mux(GPIO2_25,1);
+
+	light_pin_cfg(GPIO2_18,PIN_SPEED_NORMAL,PIN_PN,2);
+	light_pin_cfg(GPIO2_19,PIN_SPEED_NORMAL,PIN_PN,2);
+	light_pin_cfg(GPIO2_20,PIN_SPEED_NORMAL,PIN_PN,2);
+	light_pin_cfg(GPIO2_21,PIN_SPEED_NORMAL,PIN_PN,2);
+	light_pin_cfg(GPIO2_22,PIN_SPEED_NORMAL,PIN_PN,2);
+	light_pin_cfg(GPIO2_23,PIN_SPEED_NORMAL,PIN_PN,2);
+	light_pin_cfg(GPIO2_24,PIN_SPEED_NORMAL,PIN_PN,2);
+	light_pin_cfg(GPIO2_25,PIN_SPEED_NORMAL,PIN_PN,2);
+
+
+	light_pin_mux(SDIO0_WPRTN,3);
+	light_pin_mux(SDIO1_WPRTN,3);
+	light_pin_mux(SDIO1_DETN,3);
+
+	light_pin_mux(GPIO2_30,1);
+	light_pin_mux(GPIO2_31,1);
+	light_pin_mux(GPIO3_0,1);
+	light_pin_mux(GPIO3_1,1);
+
+	/*spi0 cs0 gpio2-15 pad strength and pin-pull mode*/
+	light_pin_mux(SPI_CSN,3);
+	light_pin_cfg(SPI_CSN,PIN_SPEED_NORMAL,PIN_PN,5);
+	light_pin_cfg(SPI_SCLK,PIN_SPEED_NORMAL,PIN_PN,5);
+	light_pin_cfg(SPI_MISO,PIN_SPEED_NORMAL,PIN_PU,5);
+	light_pin_cfg(SPI_MOSI,PIN_SPEED_NORMAL,PIN_PU,5);
+
+	/*qspi0 cs0 gpio2-3, pad strength and pin-pull mode  */
+	light_pin_mux(QSPI0_CSN0,3);
+	light_pin_cfg(QSPI0_CSN0,PIN_SPEED_NORMAL,PIN_PU,8);
+	light_pin_cfg(QSPI0_SCLK,PIN_SPEED_NORMAL,PIN_PN,8);
+	light_pin_cfg(QSPI0_D0_MOSI,PIN_SPEED_NORMAL,PIN_PU,8);
+	light_pin_cfg(QSPI0_D1_MISO,PIN_SPEED_NORMAL,PIN_PU,8);
+	light_pin_cfg(QSPI0_D2_WP,PIN_SPEED_NORMAL,PIN_PU,8);
+	light_pin_cfg(QSPI0_D3_HOLD,PIN_SPEED_NORMAL,PIN_PU,8);
 
 }
 
@@ -143,7 +637,7 @@ int board_init(void)
 
 	usb_clk_config();
 	gmac_clk_config();
-	spi_hw_init();
+	light_iopin_init();
 
 	clk_config();
 

commit 9a6ceb73da48de2a06f3568ef295fab701571962

board: thead: light-c910: only set io for sillicon
Only set io for sillicon.

Fixes: f275883128ad ("board: thead: support init all soc pin config")
Signed-off-by: fugang.duan <duanfugang.dfg@alibaba-inc.com>

board/thead/light-c910/light.c
@@ -213,6 +213,8 @@ enum {
 #define LIGHT_IOPMP_DEFAULT_ATTR	0xffffffff
 #define LIGHT_IOPMP_DEFAULT_OFF		0xc0
 
+#ifndef CONFIG_TARGET_LIGHT_FPGA_FM_C910
+
 #define PADMUX_REG_ADDR(base,index)  ((void*)(base) + ((index >> 3) << 2))
 #define PADMUX_REG_BIT_POS(index)    ((index & 0x7) << 2)
 #define PADMUX_REG_MASK(index)       (0xF << PADMUX_REG_BIT_POS(index))
@@ -439,6 +441,7 @@ int light_pin_cfg(pin_name_t pin_name,uint32_t slew_rate, uint32_t pullmode,uint
 
 	return ret;
 }
+#endif
 
 static struct light_iopmp_list {
         int iopmp_type;
@@ -562,6 +565,7 @@ static void usb_clk_config(void)
 	writel(0x7, (void *)USB3_DRD_SWRST);
 }
 
+#ifndef CONFIG_TARGET_LIGHT_FPGA_FM_C910
 static void light_iopin_init(void)
 {
 	light_pin_cfg(I2C_AON_SCL,PIN_SPEED_NORMAL,PIN_PN,4);
@@ -673,12 +677,16 @@ static void light_iopin_init(void)
 	light_pin_cfg(QSPI0_D3_HOLD,PIN_SPEED_NORMAL,PIN_PU,8);
 
 }
+#endif
 
 int board_init(void)
 {
 	light_iopmp_config();
 
+#ifndef CONFIG_TARGET_LIGHT_FPGA_FM_C910
 	light_iopin_init();
+#endif
+
 	clk_config();
 
 	usb_clk_config();