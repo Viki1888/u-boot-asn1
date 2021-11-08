a05bef5dd297ee24d19d35486a5d8dff60495613
fugang.duan <duanfugang.dfg@alibaba-inc.com>
2021-11-08 23:30:13 +0800

    configs/light-c910: correct the dtb file
    Correct the dtb file to light-val.dtb
    
    Signed-off-by: fugang.duan <duanfugang.dfg@alibaba-inc.com>

--- include/configs/light-c910.h
+++ include/configs/light-c910.h
@@ -35,7 +35,6 @@
 #define CONFIG_RGMII            1
 #define CONFIG_PHY_MARVELL      1
 #define CONFIG_NET_RETRY_COUNT  20
-//#define LIGHT_GMAC1_ENABLE	1
 
 #define CONFIG_SYS_FLASH_BASE       0x0
 #define CONFIG_SYS_MAX_FLASH_BANKS  1
@@ -83,7 +82,7 @@
 	"finduuid=part uuid mmc ${mmcdev}:${mmcpart} uuid\0" \
 	"gpt_partition=gpt write mmc ${mmcdev} $partitions\0" \
 	"set_bootargs=setenv bootargs console=ttyS0,115200 root=PARTUUID=${uuid} rootfstype=ext4 rdinit=/sbin/init rootwait rw earlycon clk_ignore_unused loglevel=7 eth=$ethaddr\0" \
-	"bootcmd_load=ext4load mmc 0:2 $t_opensbi_addr fw_jump.bin; ext4load mmc 0:2 $t_dtb_addr hw.dtb; ext4load mmc 0:2 $t_kernel_addr Image; ext4load mmc 0:2 $t_rootfs_addr rootfs.cpio.gz;\0" \
+	"bootcmd_load=ext4load mmc 0:2 $t_opensbi_addr fw_jump.bin; ext4load mmc 0:2 $t_dtb_addr light-val.dtb; ext4load mmc 0:2 $t_kernel_addr Image; ext4load mmc 0:2 $t_rootfs_addr rootfs.cpio.gz;\0" \
 	"bootcmd=run bootcmd_load; run finduuid; run set_bootargs; sboot $t_kernel_addr $t_rootfs_addr $t_dtb_addr;\0" \
         "\0"
 
@@ -126,7 +125,7 @@
 	"finduuid=part uuid mmc ${mmcdev}:${mmcpart} uuid\0" \
 	"gpt_partition=gpt write mmc ${mmcdev} $partitions\0" \
 	"set_bootargs=setenv bootargs console=ttyS0,115200 root=PARTUUID=${uuid} rootfstype=ext4 rdinit=/sbin/init rootwait rw earlycon clk_ignore_unused loglevel=7 eth=$ethaddr\0" \
-	"bootcmd_load=ext4load mmc 0:2 $opensbi_addr fw_jump.bin; ext4load mmc 0:2 $dtb_addr hw.dtb; ext4load mmc 0:2 $kernel_addr Image\0" \
+	"bootcmd_load=ext4load mmc 0:2 $opensbi_addr fw_jump.bin; ext4load mmc 0:2 $dtb_addr light-val.dtb; ext4load mmc 0:2 $kernel_addr Image\0" \
 	"bootcmd=run bootcmd_load; run finduuid; run set_bootargs; booti $kernel_addr - $dtb_addr;\0" \
         "\0"
 
