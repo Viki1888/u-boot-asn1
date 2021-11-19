5014556b1adaa6efbdb12105599e55a3d0d4f1e9
fugang.duan <duanfugang.dfg@alibaba-inc.com>
2021-11-19 17:49:01 +0800

    board: thead: light: bypass c910 lsu
    bypass c910 lsu.
    
    Signed-off-by: fugang.duan <duanfugang.dfg@alibaba-inc.com>

--- board/thead/light-c910/spl.c
+++ board/thead/light-c910/spl.c
@@ -85,7 +85,7 @@ void cpu_performance_enable(void)
 {
 #define CSR_MHINT2_E	0x7cc
 	csr_write(CSR_SMPEN, 0x1);
-	csr_write(CSR_MHINT2_E, csr_read(CSR_MHINT2_E) | 0x8000);
+	csr_write(CSR_MHINT2_E, csr_read(CSR_MHINT2_E) | 0x28000);
 	csr_write(CSR_MCCR2, 0xe24b0009);
 	csr_write(CSR_MHCR, 0x11ff);
 	csr_write(CSR_MXSTATUS, 0x638000);
