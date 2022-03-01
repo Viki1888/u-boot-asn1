890df72758c0610980957af4fd6192e2924560a0
vincentcui <cxx194832@alibaba-inc.com>
2022-03-01 14:59:34 +0800

    Add RPMB key for testing

--- board/thead/light-c910/boot.c
+++ board/thead/light-c910/boot.c
@@ -524,6 +524,12 @@ int light_boot(int argc, char * const argv[])
 	return 0;
 }
 
+/* the sample rpmb key is only used for testing */
+const unsigned char emmc_rpmb_key_sample[16] = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77,\
+												0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff};
+
+
+
 int csi_get_tf_image_version(unsigned int *ver)
 {
 	*ver = env_get_hex("tf_version", 0);
