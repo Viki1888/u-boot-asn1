36de136faf7814cf522d1300fbeb57fa736ebab9
You Xiao <youxiao.fc@alibaba-inc.com>
2022-03-03 17:32:44 +0800

    configs: light-c910: change splashimage to addr below 1G
    Change splashimage address below 1G in DDR to support Light B
    platform.
    
    Signed-off-by: You Xiao <youxiao.fc@alibaba-inc.com>

--- include/configs/light-c910.h
+++ include/configs/light-c910.h
@@ -79,7 +79,7 @@
 
 #ifdef CONFIG_LIGHT_SEC_BOOT
 #define CONFIG_EXTRA_ENV_SETTINGS \
-	"splashimage=0xd0000000\0" \
+	"splashimage=0x30000000\0" \
 	"splashpos=m,m\0" \
 	"t_opensbi_addr=0x100000\0" \
 	"t_kernel_addr=0x1ff800\0" \
@@ -168,7 +168,7 @@
         "\0"
 #else
 #define CONFIG_EXTRA_ENV_SETTINGS \
-	"splashimage=0xd0000000\0" \
+	"splashimage=0x30000000\0" \
 	"splashpos=m,m\0" \
 	"fdt_high=0xffffffffffffffff\0" \
 	"opensbi_addr=0x0\0" \
