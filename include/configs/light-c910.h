d06300c53510d7c34e676e064a47e15a82d21a0e
vincentcui <cxx194832@alibaba-inc.com>
2022-06-10 15:18:43 +0800

    Modify light dtb file for secure system
    Change light-a-val-sec.dtb to light-val-sec.dtb

--- include/configs/light-c910.h
+++ include/configs/light-c910.h
@@ -105,7 +105,7 @@
 	"mmcdev=0\0" \
 	"mmcpart=6\0" \
 	"uboot_version=0x0000000000000000\0"\
-	"fdt_file=light-a-val-sec.dtb\0" \
+	"fdt_file=light-val-sec.dtb\0" \
 	"uuid_rootfs=80a5a8e9-c744-491a-93c1-4f4194fd690b\0" \
 	"partitions=name=table,size=2031KB;name=boot,size=200MiB,type=boot;name=tf,size=50MiB,type=boot;name=tee,size=50MiB,type=boot;name=stash,size=50MiB,type=boot;name=root,size=4000MiB,type=linux,uuid=${uuid_rootfs};name=data,size=-,type=linux\0" \
 	"finduuid=part uuid mmc ${mmcdev}:${mmcpart} uuid\0" \
